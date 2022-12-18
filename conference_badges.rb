78a46ae40cde0c6849278c49e927e3b97e9a5876
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-12-18 02:42:15 -0600

    commit tree 5ae061f24742e0683682cd756c1257c7353fc2f1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+
+
+def badge_maker(name) 
+    "Hello, my name is #{name}."
+end
+
+badge_maker("Sam")
+
+
+
+def batch_badge_creator(names)
+    names.collect do |name| 
+    "Hello, my name is #{name}."
+    end
+end
+batch_badge_creator([])
+
+
+
+def assign_rooms(names)
+    names.collect do |name| 
+        "Hello, #{name}!
+        You'll be assigned to room #{name.index + 1}!"
+    end
+end
+
+assign_rooms([])
+
+def printer(assign_rooms)
+
+end
